d98a1fc73b47ad4bb9db949c07b305209982f8bb
jantiks <58886410+jantiks@users.noreply.github.com>
2021-02-04 15:17:32 +0400

    Initial Commit

---
+++ weeklyPayChecker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weeklyPayChecker
+//
+//  Created by Tigran on 2/4/21.
+//  Copyright © 2021 Tigran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
